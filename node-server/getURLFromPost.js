49ca0983aba98a1b320096e003266ac2903cf1ad
el3mentry <harshxsinha@gmail.com>
2023-04-24 17:16:36 +0000

    code working, minimum 12 seconds to send a post

--- node-server/getURLFromPost.js
+++ node-server/getURLFromPost.js
@@ -1,77 +1,122 @@
-const { chromium } = require('playwright');
-require('dotenv').config();
-const fs = require('fs/promises');
-const fsSync = require('fs');
+const { chromium } = require("playwright");
+require("dotenv").config();
+const fs = require("fs/promises");
+const fsSync = require("fs");
 
 function getURLFromPost(senderid) {
-    let senderIdMappedName = process.env[`${senderid}`];
-    let url = scrapeURLFromPost(senderIdMappedName);
-    return url;
+  let senderIdMappedName = process.env[`${senderid}`];
+  let url = scrapeURLFromPost(senderIdMappedName);
+  return url;
 }
 
 async function saveCookiesLocally(cookies) {
-    await fs.writeFile('./cookies.json', JSON.stringify(cookies), { encoding: 'utf-8' });
+  await fs.writeFile("./cookies.json", JSON.stringify(cookies), {
+    encoding: "utf-8",
+  });
 }
 
 async function scrapeURLFromPost(senderIdMappedName) {
-    const username = process.env.USERNAME;
-    const password = process.env.PASSWORD;
-    let url = 'None';
+  console.log(senderIdMappedName);
 
-    let browser = await chromium.launch();
-    let context = await browser.newContext();
-    let page = '';
+  const username = process.env.USERNAME;
+  const password = process.env.PASSWORD;
+  let url = "None";
 
-    if (fsSync.existsSync('./cookies.json')) {
-        let cookies = await fs.readFile('./cookies.json', { encoding: 'utf-8' });
-        await context.addCookies(JSON.parse(cookies));
-        page = await context.newPage();
-    }
-    else {
-        page = await context.newPage();
-        await page.goto('https://www.instagram.com/');
-        await page.getByLabel('Phone number, username, or email').click();
-        await page.getByLabel('Phone number, username, or email').fill(username);
-        await page.getByLabel('Password').click();
-        await page.getByLabel('Password').fill(password);
-        await page.getByRole('button', { name: 'Log in', exact: true }).click();
+  // console.time("Chrome Launch");
+  let browser = await chromium.launch();
+  let context = await browser.newContext();
+  let page = "";
+  // console.timeEnd("Chrome Launch");
 
-        await new Promise(r => setTimeout(r, 15000));
+  if (fsSync.existsSync("./cookies.json")) {
+    // console.time("Cookies Fetch");
+    let cookies = await fs.readFile("./cookies.json", { encoding: "utf-8" });
+    await context.addCookies(JSON.parse(cookies));
+    page = await context.newPage();
+    // console.timeEnd("Cookies Fetch");
+  } else {
+    console.log("trying to perform login...");
+    page = await context.newPage();
+    await page.goto("https://www.instagram.com/");
+    await page.getByLabel("Phone number, username, or email").click();
+    await page.getByLabel("Phone number, username, or email").fill(username);
+    await page.getByLabel("Password").click();
+    await page.getByLabel("Password").fill(password);
+    await page.getByRole("button", { name: "Log in", exact: true }).click();
 
-        let cookies = await context.cookies("https://www.instagram.com");
-        await saveCookiesLocally(cookies);
-    }
+    await new Promise((r) => setTimeout(r, 6000));
 
-    await page.goto('https://www.instagram.com/direct/inbox/');
-    await new Promise(r => setTimeout(r, 3000));
+    let cookies = await context.cookies("https://www.instagram.com");
+    await saveCookiesLocally(cookies);
+  }
 
-    try {
-        await page.getByRole('button', { name: 'Not Now' }).click();
-    }
-    catch (error) {
-        // console.log('Button is not present on the page!');
-        // await page.screenshot({ path: 'media/error.png' });
-    }
+  // console.time("Going to Inbox");
+  await page.goto("https://www.instagram.com/direct/inbox/");
+  await new Promise((r) => setTimeout(r, 3000));
+  // console.timeEnd("Going to Inbox");
 
-    await page.getByRole('link', {
-        name: `${senderIdMappedName}'s profile picture`, exact: true
-    }).click();
+  // try {
+  //   console.time("Not Now Button");
+  //   await page.getByRole("button", { name: "Not Now" }).click({timeout: 3000});
+  //   console.timeEnd("Not Now Button");
+  // } catch (error) {
+  //   console.log('Button is not present on the page!');
+  //   await page.screenshot({ path: 'media/error.png' });
+  // }
 
-    await new Promise(r => setTimeout(r, 2000));
+  // console.time("Profile Picture");
+  await page
+    .getByRole("link", {
+      name: `${senderIdMappedName}'s profile picture`,
+      exact: true,
+    })
+    .click();
 
-    const element = await page.getByPlaceholder('Message...');
-    const { x, y } = await element.boundingBox();
+  await new Promise((r) => setTimeout(r, 2000));
+  // console.timeEnd("Profile Picture");
 
-    await page.screenshot({ path: `media/inbox-${new Date().getTime()}.png` });
-    await page.mouse.click(x + 100, y - 100);
+  // await context.setOffline(true);
+  // console.time("Internet Block");
+  await page.route("**/*", (route) => route.abort());
+  // console.log("internet blocked");
 
-    await new Promise(r => setTimeout(r, 6000));
-    url = await page.url();
+  // console.timeEnd("Internet Block");
 
-    await context.close();
-    await browser.close();
+  // console.time("Message Box");
+  const messageBoxElement = await page.getByPlaceholder("Message...");
+  const { x, y } = await messageBoxElement.boundingBox();
+  // console.timeEnd("Message Box");
 
-    return url;
+  // console.log("trying to scroll!");
+
+  // console.time("evaluate");
+  await page.evaluate(() => {
+    document
+      .getElementsByClassName("_ab5z _ab5_")[0]
+      .scrollTo(
+        0,
+        document.getElementsByClassName("_ab5z _ab5_")[0].scrollHeight + 10000
+      );
+  });
+  // console.timeEnd("evaluate");
+
+  // console.time("MouseClick");
+  await page.screenshot({ path: `media/inbox-${new Date().getTime()}.png` });
+  await page.mouse.click(x + 100, y - 150);
+  // console.timeEnd("MouseClick");
+
+  // console.time("Url");
+  await new Promise((r) => setTimeout(r, 3000));
+  url = await page.url();
+  // console.timeEnd("Url");
+
+  // console.time("close");
+  await page.close();
+  await context.close();
+  await browser.close();
+  // console.timeEnd("close");
+
+  return url;
 }
 
-module.exports = { getURLFromPost }
+module.exports = { getURLFromPost };
--- node-server/index.js
+++ node-server/index.js
@@ -1,82 +1,77 @@
-'use strict';
+"use strict";
 // const fs = require('fs');
-const { getUrlFromMessageEntry } = require('./getUrlFromMessageEntry');
+const { getUrlFromMessageEntry } = require("./getUrlFromMessageEntry");
 
-const
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express().use(bodyParser.json()), // creates express http server
-    axios = require('axios');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  app = express().use(bodyParser.json()), // creates express http server
+  axios = require("axios");
 
 let lastTimeStamps = [];
 
 // Imports dependencies and set up http server
-app.post('/webhook', async (req, res) => {
-    let body = req.body;
-    // fs.writeFileSync('./receivedNativeObj.txt', JSON.stringify(body), { encoding: 'utf-8' });
-    
-    body.entry.forEach(async function (entry) {
-        let webhook_event = entry.messaging[0];
-        // console.log(webhook_event);
+app.post("/webhook", async (req, res) => {
+  let body = req.body;
+  // fs.writeFileSync('./receivedNativeObj.txt', JSON.stringify(body), { encoding: 'utf-8' });
 
-        // shortcode: p/alsdjfvasnd
-        let currentTimeStamp = webhook_event['timestamp'];
-        let senderid = webhook_event['sender']['id'];
+  body.entry.forEach(async function (entry) {
+    let webhook_event = entry.messaging[0];
+    // console.log(webhook_event);
+    console.log("received request!");
 
-        if (lastTimeStamps.includes(currentTimeStamp) == false) {
-            // when the 'if' condition of array containing a timestamp is false, proceess the request.
-            let url = await getUrlFromMessageEntry(webhook_event, senderid);
-            console.log(url, senderid);
-            
-            axios.post('http://localhost:5000/scrape',
-                {
-                    url: url,
-                    senderid: senderid
-                })
-                .then(res => {
-                    console.log('success');
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
+    // shortcode: p/alsdjfvasnd
+    let currentTimeStamp = webhook_event["timestamp"];
+    console.log("timestamp: ", currentTimeStamp);
+    let senderid = webhook_event["sender"]["id"];
 
-        if (lastTimeStamps.length > 40)
-            lastTimeStamps.shift();
+    if (!lastTimeStamps.includes(currentTimeStamp)) {
+      console.log(lastTimeStamps);
+      // when the 'if' condition of array containing a timestamp is false, proceess the request.
+      lastTimeStamps.push(currentTimeStamp);
 
-        lastTimeStamps.push(currentTimeStamp);
+      let url = await getUrlFromMessageEntry(webhook_event, senderid);
+      console.log(url, senderid);
+
+      // axios.post('http://localhost:5000/scrape',
+      //     {
+      //         url: url,
+      //         senderid: senderid
+      //     })
+      //     .then(res => {
+      //         console.log('success');
+      //     })
+      //     .catch(error => {
+      //         console.error(error);
+      //     });
     }
-    );
+
+    if (lastTimeStamps.length > 40) lastTimeStamps.shift();
+  });
 });
 
-
 // Adds support for GET requests to our webhook
-app.get('/webhook', (req, res) => {
+app.get("/webhook", (req, res) => {
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "Yk8wgH2na*Fy";
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "Yk8wgH2na*Fy"
+  // Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
+  }
 });
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
